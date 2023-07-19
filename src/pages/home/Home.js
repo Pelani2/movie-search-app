dda6b6b0be3802fd6e02bca475e1baf4c1d92670
Igor Zupunski <ipatheword@live.com>
2023-07-19 14:54:09 +0200

    started search section

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
+import SearchSection from "./search-section/SearchSection";
 
 export default function Home() {
     return(
         <div>
             <Navbar />
+            <SearchSection />
         </div>
     );
 }
---
+++ src/pages/home/search-section/SearchSection.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./SearchSectionStyles.css";
+
+export default function SearchSection() {
+    return(
+        <section>
+            search section
+        </section>
+    );
+}
