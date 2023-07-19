66d7c4787187c2ac42b980fbdbfc4d46851160ba
Igor Zupunski <ipatheword@live.com>
2023-07-19 14:49:30 +0200

    styled navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,14 +2,16 @@ import React from "react";
 import "./NavbarStyles.css";
 import NavbarLinkButton from "../buttons/navbar-link-button/NavbarLinkButton";
 import AuthButton from "../buttons/auth-buttons/AuthButton";
+import NavbarLogo from "../../assets/logos/logo.png";
 
 export default function Navbar() {
     return(
         <nav className="navbar">
             <div className="navbar__logo">
-                <a href="/">
-                    My Logo
-                </a>
+                <img 
+                    alt="movielogo"
+                    src={NavbarLogo}
+                />
             </div>
 
             <ul className="navbar__links"> 
