22fe0aa6d9179b06b631ebea867a893b4fe918c8
Igor Zupunski <ipatheword@live.com>
2023-07-20 23:22:47 +0200

    small changes to everything

--- src/pages/home/search-section/SearchSection.js
+++ src/pages/home/search-section/SearchSection.js
@@ -1,15 +1,34 @@
 import React, { useState } from "react";
 import "./SearchSectionStyles.css";
 import SearchMovieButton from "../../../components/buttons/search-movie-button/SearchMovieButton";
+import axios from "axios";
+import "./SearchResultsStyles.css";
+import { Link } from "react-router-dom";
 
 export default function SearchSection() {
+    const API_KEY = "4737e7948022809d14a75ad5e7df82c1";
+    const API_URL = "https://api.themoviedb.org/3/search/movie";
+
     const [searchText, setSearchText] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
 
     const handleSearch = () => {
-        // API LOGIC HERE
-
-        console.log(`Performing search for : ${searchText}`);
+        if (searchText.trim() === "") {
+            setSearchResults([]);
+            return;
+        }
     };
+
+    axios.get(API_URL, {
+        params: {
+            api_key: API_KEY,
+            query: searchText.trim(),
+        }
+    }).then((response) => {
+        setSearchResults(response.data.results);
+    }).catch((error) => {
+        console.error(`Error fetching search results: ${error}`);
+    });
 
     const handleInputChange = (event) => {
         setSearchText(event.target.value);
@@ -32,6 +51,29 @@ export default function SearchSection() {
                         clickFunc={handleSearch}
                     />
                 </div>
+                <section className="movie-container">
+                    <div className="movie-search-container">
+                        <div className="search-results">
+                            {searchResults.length > 0 ? (
+                                <ul>
+                                    {searchResults.map((movie) => (
+                                        <Link 
+                                            key={movie.id}
+                                            className="dropped-movie"
+                                            to="/moviedetails"
+                                        >
+                                            {movie.title}
+                                        </Link>
+                                    ))}
+                                </ul>
+                            ) : (
+                                <p>
+                                    No search results found.
+                                </p>
+                            )}
+                        </div>
+                    </div>
+                </section>
         </section>
     );
 }
